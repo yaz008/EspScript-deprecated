c523c4baf7365b1e42093b2b3a21ec37bc55a4fb
yaz008 <yaz008.yaz008@yandex.ru>
2024-06-21 02:31:01 +0300

    Update repeat

--- src/scripts/repeat.py
+++ src/scripts/repeat.py
@@ -1,2 +1,2 @@
-def repeat(string: str, times: str) -> str:
-    return string * int(times)
+def repeat(string: str, times: str, sep: str = '') -> str:
+    return f'{string}{sep}' * (int(times) - 1) + string
