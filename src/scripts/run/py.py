f3c6ee6e3b5baf327815eada3915897537630b32
yaz008 <yaz008.yaz008@yandex.ru>
2024-06-22 11:36:39 +0300

    Add scripts.run.py::evaluate
    Minor changes:
        Fix run type annotations

--- src/scripts/run/py.py
+++ src/scripts/run/py.py
@@ -6,7 +6,7 @@ def set_file(file: str, content: str) -> None:
     with open(file=file, mode='w', encoding='UTF-8') as TmpFile:
         TmpFile.write(content)
 
-def run_python_code(stdin: str, *args: str) -> Any:
+def run_python_code(stdin: str, *args: str) -> str:
     tmp: str = f'{'\\'.join(path[0].split(sep='\\')[:-1])}\\temp\\tmp.py'
     set_file(file=tmp, content=stdin)
     result: CompletedProcess = run(f'python {tmp} {' '.join([f'\"{arg}\"' for arg in args])}',
@@ -14,3 +14,7 @@ def run_python_code(stdin: str, *args: str) -> Any:
                                    text=True)
     set_file(file=tmp, content='')
     return result.stdout if bool(result.stdout) else None
+
+def evaluate(stdin: str, *args: str) -> Any:
+    import math
+    return eval(stdin,math.__dict__)
