e249e3a5d2a3a0d7277938ea4aa3ae5af1a9ee11
yaz008 <yaz008.yaz008@yandex.ru>
2024-06-22 10:14:29 +0300

    Add standard casts
    scripts.cast.type_conversion::cast_to_string
    scripts.cast.type_conversion::cast_to_int
    scripts.cast.type_conversion::cast_to_float

---
+++ src/scripts/cast/type_conversion.py
@@ -0,0 +1,8 @@
+def cast_to_string(stdin: str, *args: str) -> int:
+    return int(stdin)
+
+def cast_to_int(stdin: str, *args: str) -> int:
+    return int(stdin)
+
+def cast_to_float(stdin: str, *args: str) -> int:
+    return float(stdin)
