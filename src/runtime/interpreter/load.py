496b139d93b4055698d3879b146881f9f3555ee5
yaz008 <yaz008.yaz008@yandex.ru>
2024-06-25 07:04:18 +0300

    Add json loader instead of yaml

--- src/runtime/interpreter/load.py
+++ src/runtime/interpreter/load.py
@@ -1,12 +1,12 @@
 from sys import path
 from importlib import import_module
-from yaml import load, SafeLoader
+from json import load
 from types import ModuleType
 from typing import Callable, Any
 
 config: str = f'{'\\'.join(path[0].split(sep='\\')[:-1])}\\config.json'
 with open(file=config, mode='r', encoding='UTF-8') as ConfigFile:
-    lookup: dict[str, str] = load(ConfigFile, SafeLoader)
+    lookup: dict[str, str] = load(ConfigFile)
 
 def load_by_name(name: str) -> Callable[..., Any]:
     path, funcname = lookup[name].split(sep='::')
